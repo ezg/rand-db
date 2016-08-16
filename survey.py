ec12603fd7205a0e1f3785e48222f94ae41d7d93
Sam Zhao <sam@cs.brown.edu>
2016-08-16 17:03:06 -0400

    survey: add pairwise pearson correlation

--- survey.py
+++ survey.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python
 import csv
+from scipy.stats.stats import pearsonr
 
 survey_path = './survey.csv'
+
+checkbox_attrs = ['Answer.FavoriteFood', 'Answer.Pet', 'Answer.Fruit', 'Answer.Politician', 'Answer.MusicGenre', 'Answer.SocialMedia', 'Answer.ColorSocks', 'Answer.FootballTeam', 'Answer.MusicInstrument', 'Answer.Sport', 'Answer.Event']
+
+number_attrs = ['Answer.HolidayDestination', 'Answer.Age', 'Answer.Height', 'Answer.Siblings', 'Answer.Pillow', 'Answer.ChildrenWant', 'Answer.CreditCard', 'Answer.Car', 'Answer.ChildrenHave', 'Answer.CountriesBeenTo', 'Answer.BookRead', 'Answer.BookOwned', 'Answer.Coffee', 'Answer.InternetUsage', 'Answer.TV', 'Answer.ApplePerMonth', 'Answer.ExerciseWeekly']
+
+id_attrs = ['assignment-id']
+
+radio_attrs = ['Answer.Alien', 'Answer.CityOrSuburb', 'Answer.marijuana', 'Answer.HairDrying', 'Answer.MailService', 'Answer.Brexit', 'Answer.SQL', 'Answer.Education', 'Answer.NuclearEnergy', 'Answer.GameOfThrones', 'Answer.Snow', 'Answer.LikeCountry', 'Answer.Unicorn', 'Answer.CarTransmission', 'Answer.InternetBrowser', 'Answer.Database', 'Answer.Tennis', 'Answer.Smoke', 'Answer.Bluegrass', 'Answer.EyeColor', 'Answer.PokemonGo', 'Answer.Sudoku', 'Answer.WritingHand', 'Answer.GMO', 'Answer.Skydiving', 'Answer.BathOrShower', 'Answer.Vacation', 'Answer.StartupOrCorporation', 'Answer.Olympics', 'Answer.GunControl', 'Answer.Religious', 'Answer.Potato', 'Answer.Continent', 'Answer.Juggle', 'Answer.PhoneBrand', 'Answer.MaritalStatus', 'Answer.GlobalEconomy', 'Answer.Sauna', 'Answer.OnlineShopping', 'Answer.Lesson', 'Answer.Kindle', 'Answer.SmartphoneOS', 'Answer.FlightSeat', 'Answer.DinerWith', 'Answer.HouseHoldIncome', 'Answer.Stonebraker', 'Answer.Drunk', 'Answer.JumpOnOneFoot', 'Answer.Rain', 'Answer.JobMoneyOrFun', 'Answer.Gender', 'Answer.GlobalWarming', 'Answer.ScaryMovie', 'Answer.CuteAnimal', 'Answer.Darwin', 'Answer.ElectricOrGasCar', 'Answer.LeiaOrSkywalker', 'Answer.RentOrBuyHouse', 'Answer.Cook', 'Answer.earlobe', 'Answer.DNA', 'Answer.DrinkForDinner', 'Answer.Gym', 'Answer.SunriseOrSunset', 'Answer.Paris', 'Answer.Astrology', 'Answer.Film', 'Answer.HairColor', 'Answer.Newspaper']
+
+def main():
+    survey = load_survey_table()
+    #print(survey)
+    #attr_set = set(survey.attrs)
+    #assert set(checkbox_attrs) < attr_set
+    #assert set(text_attrs) < attr_set
+    #assert set(number_attrs) < attr_set
+    #print(attr_set - set(checkbox_attrs) - set(text_attrs) - set(number_attrs))
+    #print(survey.col_at(id_attrs[0]))
+
+    #ages = list(map(int, survey.col_at('Answer.Age')))
+    #city_or_suburbs = list(map(int, survey.col_at('Answer.CityOrSuburb')))
+    #ans = pearsonr(ages, city_or_suburbs)
+    #print(ans)
+
+    pearsons = dict()
+    test_attrs = set(radio_attrs + number_attrs)
+    for x_attr in test_attrs:
+        x_col = list(map(int, survey.col_at(x_attr)))
+        for y_attr in (test_attrs - set([x_attr])):
+            if (x_attr, y_attr) not in pearsons and (y_attr, x_attr) not in pearsons:
+                y_col = list(map(int, survey.col_at(y_attr)))
+                # pearsonr(x, y) -> (correl-coef, p-value)
+                pearsons[(x_attr, y_attr)] = pearsonr(x_col, y_col)
+
+    sorted_pearsons = sorted(list(pearsons.items()), key=lambda p: p[1][1], reverse=False)
+    #print(sorted_pearsons)
+    for p in sorted_pearsons:
+        print(p[0][0], ',', p[0][1], ',', p[1][0], ',', p[1][1])
+
 
 # output:
 #   assignment-id: col-num
@@ -14,6 +54,19 @@ def check_empty_cell():
             f_row = filter(lambda x: not x, row)
             if list(f_row):
                 print('{}: {}'.format(row[0], row.index('')))
+
+
+def load_survey_table():
+    with open(survey_path, newline='') as file:
+        reader = csv.reader(file, delimiter=',', quotechar='"')
+
+        rows = [row for row in reader]
+        assert len(rows) > 1
+        # first row is column ids
+        # second row is column names
+        attrs = rows[1]
+        mtrx = rows[2:]
+        return Table(attrs, mtrx)
 
 
 class Table:
@@ -35,6 +88,12 @@ class Table:
     def row_at(self, row):
         assert 0 <= row and row < self.num_rows
         return self.mtrx[row]
+
+    def col_at(self, attr):
+        assert attr in self.attrs
+        col = self.attrs.index(attr)
+        return [row[col] for row in self.mtrx]
+
 
     def project(self, attrs):
         cols = [self.attrs.index(attr) for attr in attrs]
@@ -81,8 +140,7 @@ class Table:
         return res
 
 
-def main():
-    #check_empty_cell()
+def test_table():
     mtrx = [
         [1, 2, 3, 4],
         [1 * 2, 2 * 2, 3 * 2, 4 * 2],
@@ -105,7 +163,8 @@ def main():
 
     print(table.filter(lambda t: t.at('c') % 2 is 0))
 
-    print(table.group_aggregate(['d', 'b'], ['a', 'c'], lambda table: table.num_rows, 'count'))
+    print(table.group_aggregate(['d', 'b'], ['a', 'c'], lambda t: t.num_rows, 'count'))
+
 
 
 if __name__ == '__main__':
